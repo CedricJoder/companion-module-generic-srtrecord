d97146913b51ce349a8b9ded5676af190d316de6
CedricJoder <cedric.joder@gmail.com>
2024-06-03 04:18:56 +0200

    preset

--- src/presets.js
+++ src/presets.js
@@ -7,11 +7,11 @@ module.exports = {
 const presets = {}
 presets[`Record`] = {
 	type: 'button', // This must be 'button' for now
-	category: '', // This groups presets into categories in the ui. Try to create logical groups to help users find presets
+	category: 'Recording', // This groups presets into categories in the ui. Try to create logical groups to help users find presets
 	name: `Record - auto write`, // A name for the preset. Shown to the user when they hover over it
 	style: {
 		// This is the minimal set of style properties you must define
-		text: self.running ? 'Recording' : 'Record'
+		text: self.running ? 'Recording' : 'Record',
 //`$(generic-module:some-variable)`, // You can use variables from your module here
 		size: 'auto',
 		color: combineRgb(255, 255, 255),
@@ -22,26 +22,36 @@ presets[`Record`] = {
 			down: [
 				{
 					// add an action on down press
-					actionId: 'record',
-					options: {
-						// options values to use
-					},
+					actionId: 'startRecording',
+					options: {},
 				},
 			],
 			up: [],
+			
+		3000: {
+				actions : [
+					{
+						actionId: 'stopRecording',
+						options: {},
+					},
+				],
+				options: {
+					runWhileHeld: true,
+				},
+			}	
 		},
 	],
 	feedbacks: [
 		{
-			feedbackId: 'autowrite'
+			feedbackId: 'autoWrite',
 		},
 		{
-			feedbackId: 'status'
+			feedbackId: 'isRecording',
 		},
 	], // You can add some presets from your module here
 }
 this.setPresetDefinitions(presets)
-
+	
 		this.setPresetDefinitions(presets)
 	},
 }
